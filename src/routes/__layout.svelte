7d4cd1f2c78b4f0b43569b3a559b6911b9898f9e
Birdger Tower <57158102+brijr@users.noreply.github.com>
2022-06-28 09:58:15 -0600

    Fix Icon

--- src/routes/__layout.svelte
+++ src/routes/__layout.svelte
@@ -2,6 +2,10 @@
     import "../app.css";
 </script>
 
+<svelte:head>
+    <link rel="icon" href="./favicon.png"/>
+</svelte:head>
+
 <body class="p-4 bg-[#222] max-w-screen-sm m-auto">
     <slot />
 </body>
